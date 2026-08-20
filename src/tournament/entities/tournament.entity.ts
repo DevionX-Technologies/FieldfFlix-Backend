ff0d18e10457f6918f02fd4794fa995203e8df3c
Aniruddh <144269616+Aniruddh-fullstac@users.noreply.github.com>
2026-08-21 04:35:33 +0530

    feat: Support Base64 images for tournaments

--- src/tournament/entities/tournament.entity.ts
+++ src/tournament/entities/tournament.entity.ts
@@ -24,7 +24,7 @@ export class TournamentEntity {
   @Column({ type: 'varchar', length: 100, default: 'Pickleball' })
   sport: string;
 
-  @Column({ type: 'varchar', length: 500, nullable: true })
+  @Column({ type: 'text', nullable: true })
   bannerImage: string;
 
   @Column({ type: 'integer', default: 0 })
