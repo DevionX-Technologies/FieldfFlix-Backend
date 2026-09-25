aae0086f500f166ccaf28324e8cbe244caca49ae
Lokesh Konka <konkalokesh372@gmail.com>
2026-09-25 10:24:49 +0000

    data sources

--- db/data-source.ts
+++ db/data-source.ts
@@ -29,11 +29,15 @@ function migrationGlobs(): string[] {
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE ?? 'fieldflicks-dev',
+  ...(process.env.DATABASE_URL
+    ? { url: process.env.DATABASE_URL }
+    : {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT ? +process.env.DB_PORT : 5432,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE ?? 'neondb',
+      }),
   entities: entityGlobs(),
   migrations: migrationGlobs(),
   // ...(process.env.ENVIRONMENT !== 'development'
